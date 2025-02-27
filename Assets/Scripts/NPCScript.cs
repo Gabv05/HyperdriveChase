f58275a34f46e991a86debfe5ae4583cb90c7f50
Gabv05 <gabv2005@gmail.com>
2025-02-27 19:20:53 +0000

    NPC Update

--- Assets/Scripts/NPCScript.cs
+++ Assets/Scripts/NPCScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48b72a1587353a3b22eb2f222377eb5d13520db9c0d6c4f1ad37d3dc0f9c1fa9
-size 2512
+oid sha256:cc8ea4ab39a636cc76ed8cbee164d214fffd7e092586c2d7d5905f2019cd84b3
+size 3374
